94c9d46e04db903a970d816871cf70b7503e713f
eunsup <eunsup777@naver.com>
2020-11-09 16:44:48 +0900

    VO 파일 추가(eduApply)

---
+++ ibk-itep-maven/src/main/java/com/ibk/itep/vo/eduApply/NewEduApplyVo.java
@@ -0,0 +1,140 @@
+package com.ibk.itep.vo.eduApply;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * @author EUNSUP
+ *
+ */
+public class NewEduApplyVo {
+	
+	private Integer aplcId;
+	private String userId;
+	private String aplcTs;
+	private String inbkEdctYn;
+	private String edctNm;
+	private String edctCon;
+	private String edinNm;
+	private Date edctSttgYmd;
+	private Date edctFnshYmd;
+	private Date aplcSttgYmd;
+	private Date aplcFnshYmd;
+	private Time edctSttgTim;
+	private Time edctFnshTim;
+	private String edex;
+	private String onlEdctYn;
+	private String edctLevl;
+	private String apndDat;
+	private String cnfmId;
+	
+	public Integer getAplcId() {
+		return aplcId;
+	}
+	public void setAplcId(Integer aplcId) {
+		this.aplcId = aplcId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getAplcTs() {
+		return aplcTs;
+	}
+	public void setAplcTs(String aplcTs) {
+		this.aplcTs = aplcTs;
+	}
+	public String getInbkEdctYn() {
+		return inbkEdctYn;
+	}
+	public void setInbkEdctYn(String inbkEdctYn) {
+		this.inbkEdctYn = inbkEdctYn;
+	}
+	public String getEdctNm() {
+		return edctNm;
+	}
+	public void setEdctNm(String edctNm) {
+		this.edctNm = edctNm;
+	}
+	public String getEdctCon() {
+		return edctCon;
+	}
+	public void setEdctCon(String edctCon) {
+		this.edctCon = edctCon;
+	}
+	public String getEdinNm() {
+		return edinNm;
+	}
+	public void setEdinNm(String edinNm) {
+		this.edinNm = edinNm;
+	}
+	public Date getEdctSttgYmd() {
+		return edctSttgYmd;
+	}
+	public void setEdctSttgYmd(Date edctSttgYmd) {
+		this.edctSttgYmd = edctSttgYmd;
+	}
+	public Date getEdctFnshYmd() {
+		return edctFnshYmd;
+	}
+	public void setEdctFnshYmd(Date edctFnshYmd) {
+		this.edctFnshYmd = edctFnshYmd;
+	}
+	public Date getAplcSttgYmd() {
+		return aplcSttgYmd;
+	}
+	public void setAplcSttgYmd(Date aplcSttgYmd) {
+		this.aplcSttgYmd = aplcSttgYmd;
+	}
+	public Date getAplcFnshYmd() {
+		return aplcFnshYmd;
+	}
+	public void setAplcFnshYmd(Date aplcFnshYmd) {
+		this.aplcFnshYmd = aplcFnshYmd;
+	}
+	public Time getEdctSttgTim() {
+		return edctSttgTim;
+	}
+	public void setEdctSttgTim(Time edctSttgTim) {
+		this.edctSttgTim = edctSttgTim;
+	}
+	public Time getEdctFnshTim() {
+		return edctFnshTim;
+	}
+	public void setEdctFnshTim(Time edctFnshTim) {
+		this.edctFnshTim = edctFnshTim;
+	}
+	public String getEdex() {
+		return edex;
+	}
+	public void setEdex(String edex) {
+		this.edex = edex;
+	}
+	public String getOnlEdctYn() {
+		return onlEdctYn;
+	}
+	public void setOnlEdctYn(String onlEdctYn) {
+		this.onlEdctYn = onlEdctYn;
+	}
+	public String getEdctLevl() {
+		return edctLevl;
+	}
+	public void setEdctLevl(String edctLevl) {
+		this.edctLevl = edctLevl;
+	}
+	public String getApndDat() {
+		return apndDat;
+	}
+	public void setApndDat(String apndDat) {
+		this.apndDat = apndDat;
+	}
+	public String getCnfmId() {
+		return cnfmId;
+	}
+	public void setCnfmId(String cnfmId) {
+		this.cnfmId = cnfmId;
+	}
+
+}
