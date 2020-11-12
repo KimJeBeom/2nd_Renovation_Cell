92d759c1e34e7af5941ebb5b056d508e072ff401
hanws7 <hanws77@naver.com>
2020-11-12 10:49:32 +0900

    cmm관련 vo, admin 관련 vo 추가

---
+++ ibk-itep-maven/src/main/java/com/ibk/itep/vo/admin/EduRegModVo.java
@@ -0,0 +1,45 @@
+package com.ibk.itep.vo.admin;
+
+public class EduRegModVo {
+	
+	private String 	edctClsfNm;	//교육분류명
+	private Integer	edctId; 	//교육메인ID
+	private String 	edctNm;		//교육명
+	private String 	edinNm;		//교육기관명
+	private String 	edctClsfCd;	//교육분류코드
+	
+	
+	public String getEdctClsfNm() {
+		return edctClsfNm;
+	}
+	public void setEdctClsfNm(String edctClsfNm) {
+		this.edctClsfNm = edctClsfNm;
+	}
+	public Integer getEdctId() {
+		return edctId;
+	}
+	public void setEdctId(Integer edctId) {
+		this.edctId = edctId;
+	}
+	public String getEdctNm() {
+		return edctNm;
+	}
+	public void setEdctNm(String edctNm) {
+		this.edctNm = edctNm;
+	}
+	public String getEdinNm() {
+		return edinNm;
+	}
+	public void setEdinNm(String edinNm) {
+		this.edinNm = edinNm;
+	}
+	public String getEdctClsfCd() {
+		return edctClsfCd;
+	}
+	public void setEdctClsfCd(String edctClsfCd) {
+		this.edctClsfCd = edctClsfCd;
+	}
+
+	
+	
+}
---
+++ ibk-itep-maven/src/main/java/com/ibk/itep/vo/cmm/cliVo.java
@@ -0,0 +1,26 @@
+package com.ibk.itep.vo.cmm;
+
+public class cliVo {
+	private String	edinCd;	// 교육기관코드
+	private String	edinNm;	// 교육기관명									
+	private String	useYn;	// 사용여부
+	
+	public String getEdinCd() {
+		return edinCd;
+	}
+	public void setEdinCd(String edinCd) {
+		this.edinCd = edinCd;
+	}
+	public String getEdinNm() {
+		return edinNm;
+	}
+	public void setEdinNm(String edinNm) {
+		this.edinNm = edinNm;
+	}
+	public String getUseYn() {
+		return useYn;
+	}
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}	
+}
