94ba9dc3dcc91e98c7944a669834bbf69166017b
hanws7 <hanws77@naver.com>
2020-11-16 15:50:20 +0900

    교육 조회 결과 ajax 비동기 갱신 부 구현

--- ibk-itep-maven/src/main/java/com/ibk/itep/service/admin/EduRegModService.java
+++ ibk-itep-maven/src/main/java/com/ibk/itep/service/admin/EduRegModService.java
@@ -1,5 +1,21 @@
 package com.ibk.itep.service.admin;
 
-public class EduRegModService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibk.itep.repository.AdminDao;
+import com.ibk.itep.vo.admin.EduRegModVo;
+
+@Service
+public class EduRegModService {
+	
+	@Autowired
+	private AdminDao adminDao;
+	
+	public List<EduRegModVo> selectEduRegMod(EduRegModVo vo){
+		return adminDao.selectEduRegMod(vo);
+	}
 }
+
