a6e7777cb7beec60327bbc3ccdd2f0cfde7ee435
devKJY <wldudmanya@naver.com>
2020-11-24 15:53:58 +0900

    CmbVo 추가

---
+++ ibk-itep-maven/src/main/java/com/ibk/itep/vo/cmm/CmbVo.java
@@ -0,0 +1,26 @@
+package com.ibk.itep.vo.cmm;
+
+public class CmbVo {
+	private String	brcd;		// 부점코드
+	private String	brnm;		// 부점명								
+	private String	hgrn_brcd;	// 상위부점코드
+	
+	public String getBrcd() {
+		return brcd;
+	}
+	public void setBrcd(String brcd) {
+		this.brcd = brcd;
+	}
+	public String getBrnm() {
+		return brnm;
+	}
+	public void setBrnm(String brnm) {
+		this.brnm = brnm;
+	}
+	public String getHgrn_brcd() {
+		return hgrn_brcd;
+	}
+	public void setHgrn_brcd(String hgrn_brcd) {
+		this.hgrn_brcd = hgrn_brcd;
+	}
+}
