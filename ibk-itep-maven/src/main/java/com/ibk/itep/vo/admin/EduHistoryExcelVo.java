37df3424283eb333f92416efbe61c021d812881a
devKJY <wldudmanya@naver.com>
2020-12-01 09:36:02 +0900

    EduCdMngService, EduHistoryExcelVo add

---
+++ ibk-itep-maven/src/main/java/com/ibk/itep/vo/admin/EduHistoryExcelVo.java
@@ -0,0 +1,93 @@
+package com.ibk.itep.vo.admin;
+
+public class EduHistoryExcelVo {
+	
+	private Integer	edctAplcId;	// 교육 신청 ID
+	private String	edctClsfNm; // 교육 분류
+	private String	brnm; 		// 부서명
+	private String 	userNm; 	// 직원명
+	private String 	userId; 	// 직원번호
+	private Integer edctId;		// 교육 ID
+	private String 	edctNm; 	// 교육명
+	private String 	edinNm; 	// 교육기관명
+	private String 	edctSttgYmd;// 교육 시작일
+	private String 	edctFnshYmd;// 교육 종료일
+	private String 	edex; 		// 예산
+	private String 	rfrcCon; 	// 참고
+	
+	public Integer getEdctAplcId() {
+		return edctAplcId;
+	}
+	public void setEdctAplcId(Integer edctAplcId) {
+		this.edctAplcId = edctAplcId;
+	}
+	public Integer getEdctId() {
+		return edctId;
+	}
+	public void setEdctId(Integer edctId) {
+		this.edctId = edctId;
+	}
+	public String getEdctClsfNm() {
+		return edctClsfNm;
+	}
+	public void setEdctClsfNm(String edctClsfNm) {
+		this.edctClsfNm = edctClsfNm;
+	}
+	public String getBrnm() {
+		return brnm;
+	}
+	public void setBrnm(String brnm) {
+		this.brnm = brnm;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getEdctNm() {
+		return edctNm;
+	}
+	public void setEdctNm(String edctNm) {
+		this.edctNm = edctNm;
+	}
+	public String getEdinNm() {
+		return edinNm;
+	}
+	public void setEdinNm(String edinNm) {
+		this.edinNm = edinNm;
+	}
+	public String getEdctSttgYmd() {
+		return edctSttgYmd;
+	}
+	public void setEdctSttgYmd(String edctSttgYmd) {
+		this.edctSttgYmd = edctSttgYmd;
+	}
+	public String getEdctFnshYmd() {
+		return edctFnshYmd;
+	}
+	public void setEdctFnshYmd(String edctFnshYmd) {
+		this.edctFnshYmd = edctFnshYmd;
+	}
+	public String getEdex() {
+		return edex;
+	}
+	public void setEdex(String edex) {
+		this.edex = edex;
+	}
+	public String getRfrcCon() {
+		return rfrcCon;
+	}
+	public void setRfrcCon(String rfrcCon) {
+		this.rfrcCon = rfrcCon;
+	}
+	
+	
+	
+}
